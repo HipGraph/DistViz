11a5bb1c7946e81ac60529fd527060136c7e2b66
Isuru Ranawaka <irjanith@gmail.com>
2023-11-16 10:38:46 -0500

    Add MRPT code

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,25 +1,42 @@
 #include "lib/Mrpt.h"
 #include <iostream>
 #include "Eigen/Dense"
-
+#include "io/file_reader.hpp"
 using namespace std;
 
 int main(int argc, char* argv[]) {
 
-  int n = 10000, d = 200, k = 10;
+  string input_path = "";
+
+  for (int p = 0; p < argc; p++)
+  {
+    if (strcmp(argv[p], "-input") == 0)
+    {
+      input_path = argv[p + 1];
+    }
+  }
+
+  int k = 10;
   double target_recall = 0.9;
-  Eigen::MatrixXf X = Eigen::MatrixXf::Random(d, n);
-  Eigen::MatrixXf q = Eigen::VectorXf::Random(d);
+//  int n = 10000, d = 200, k = 10;
+//  double target_recall = 0.9;
+//  Eigen::MatrixXf X = Eigen::MatrixXf::Random(d, n);
+//  Eigen::MatrixXf q = Eigen::VectorXf::Random(d);
 
-  Eigen::VectorXi indices(k), indices_exact(k);
 
-  Mrpt::exact_knn(q, X, k, indices_exact.data());
-  std::cout << indices_exact.transpose() << std::endl;
+  hipgraph::distviz::FileReader file_reader;
+  Eigen::MatrixXi indices(k);
+
+  Eigen::MatrixXf* X = file_reader.load_data(input_path);
+
+//
+//  Mrpt::exact_knn(q, X, k, indices_exact.data());
+//  std::cout << indices_exact.transpose() << std::endl;
 
   Mrpt mrpt(X);
   mrpt.grow_autotune(target_recall, k);
 
-  mrpt.query(q, indices.data());
-  std::cout << indices.transpose() << std::endl;
+  mrpt.query(X, indices.data());
+  std::cout << indices << std::endl;
 
 }
