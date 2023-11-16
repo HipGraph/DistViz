e805099ccd3b7acdadd82aa7aa41c76dabbf4c4d
Isuru Ranawaka <irjanith@gmail.com>
2023-11-16 09:25:24 -0500

    Add MRPT code

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,5 +1,25 @@
 #include "lib/Mrpt.h"
+#include <iostream>
+#include "Eigen/Dense"
+
+using namespace std;
 
 int main(int argc, char* argv[]) {
-  return 0;
+
+  int n = 10000, d = 200, k = 10;
+  double target_recall = 0.9;
+  Eigen::MatrixXf X = Eigen::MatrixXf::Random(d, n);
+  Eigen::MatrixXf q = Eigen::VectorXf::Random(d);
+
+  Eigen::VectorXi indices(k), indices_exact(k);
+
+  Mrpt::exact_knn(q, X, k, indices_exact.data());
+  std::cout << indices_exact.transpose() << std::endl;
+
+  Mrpt mrpt(X);
+  mrpt.grow_autotune(target_recall, k);
+
+  mrpt.query(q, indices.data());
+  std::cout << indices.transpose() << std::endl;
+
 }
