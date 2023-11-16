0af3ea0107bd0cc2e07aeca2b38411c4c43764de
Isuru Ranawaka <irjanith@gmail.com>
2023-11-16 12:05:22 -0500

    Add MRPT code

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -33,7 +33,7 @@ int main(int argc, char* argv[]) {
   std::cout << "Number of Rows: " << X.rows() << std::endl;
   std::cout << "Number of Columns: " << X.cols() << std::endl;
 
-  std::cout << X.row(0).transpose() << std::endl;
+  std::cout << X.row(0) << std::endl;
 
 //
   Mrpt::exact_knn(X.row(0), X, k, indices_exact.data());
