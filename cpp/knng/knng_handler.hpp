06d92f60ae1c7bc72281a65613a874263799377b
Isuru Ranawaka <irjanith@gmail.com>
2023-11-18 19:25:23 -0500

    Add MRPT code

--- cpp/knng/knng_handler.hpp
+++ cpp/knng/knng_handler.hpp
@@ -114,7 +114,7 @@ public:
       cout << " rank " << grid->rank_in_col << "  tree leaf correlation completed " << endl;
     }
 
-    shared_ptr<DataNode3DVector<INDEX_TYPE,VALUE_TYPE>>* leaf_nodes_of_trees_ptr(ntrees);
+    shared_ptr<DataNode3DVector<INDEX_TYPE,VALUE_TYPE>>* leaf_nodes_of_trees_ptr =  make_shared<DataNode3DVector<INDEX_TYPE,VALUE_TYPE>>(ntrees);
 
     cout << " rank " << grid->rank_in_col << " running  datapoint collection  "<< endl;
     // running the similar datapoint collection
