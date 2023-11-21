fb9eefbab4d4572779ce4efe5788b2da01445617
Isuru Ranawaka <irjanith@gmail.com>
2023-11-20 23:22:05 -0500

    Add MRPT code

--- cpp/knng/knng_handler.hpp
+++ cpp/knng/knng_handler.hpp
@@ -146,7 +146,7 @@ public:
 //       edge.dst_index = (*datamap_ptr)[tempRow[k]];
        edge.dst_index = tempRow[k];
        edge.distance = tempDis[k];
-//       (*local_nn_map_ptr)[global_index][k]=edge;
+       (*local_nn_map_ptr)[global_index][k]=edge;
       }
     }
 
