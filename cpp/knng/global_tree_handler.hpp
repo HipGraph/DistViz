3220ed3f6ba5bf8f49f9d0eccc5c0c618f7a078d
Isuru Ranawaka <irjanith@gmail.com>
2023-11-22 12:40:01 -0500

    Add MRPT code

--- cpp/knng/global_tree_handler.hpp
+++ cpp/knng/global_tree_handler.hpp
@@ -623,7 +623,7 @@ public:
       }
     }
 
-    MPI_Alltoallv (send_indices_ptr,(*send_indices_count_ptr).data(),(*send_disps_indices_count_ptr).data() , MPI_INDEX_TYPE,receive_indices_ptr), (*receive_indices_count_ptr).data(),
+    MPI_Alltoallv(send_indices_ptr,(*send_indices_count_ptr).data(),(*send_disps_indices_count_ptr).data() , MPI_INDEX_TYPE,receive_indices_ptr, (*receive_indices_count_ptr).data(),
                   (*receive_disps_indices_count_ptr).data(),MPI_INDEX_TYPE, grid->col_world);
 
 //    MPI_Alltoallv ((*send_values_ptr).data(),(*send_values_count_ptr).data(),
