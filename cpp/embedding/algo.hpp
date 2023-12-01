18644d643bb87386f5bdd619596bc4d067af7957
Isuru Ranawaka <irjanith@gmail.com>
2023-12-01 17:04:27 -0500

    Adding fvec reader

--- cpp/embedding/algo.hpp
+++ cpp/embedding/algo.hpp
@@ -180,12 +180,15 @@ public:
           this->calc_t_dist_grad_rowptr(csr_block, prevCoordinates, lr, j,
                                         batch_size, considering_batch_size,
                                         true, false, 0, 0, false);
+          cout << " rank " << grid->rank_in_col << " local completed " << batches << endl;
 
           this->calc_t_dist_replus_rowptr(prevCoordinates, random_number_vec,
                                           lr, j, batch_size,
                                           considering_batch_size);
+          cout << " rank " << grid->rank_in_col << " replsive completed " << batches << endl;
 //
           batch_error += this->update_data_matrix_rowptr(prevCoordinates, j, batch_size);
+          cout << " rank " << grid->rank_in_col << " update completed " << batches << endl;
 
         } else {
           //These operations are for more than one processes.
