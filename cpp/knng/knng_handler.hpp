3b9a1d25befc8fccbaa03693a45d44071738edf6
Isuru Ranawaka <irjanith@gmail.com>
2023-12-09 10:02:09 -0500

    remove mkl baxed CSR creation

--- cpp/knng/knng_handler.hpp
+++ cpp/knng/knng_handler.hpp
@@ -235,7 +235,7 @@ public:
         Tuple<VALUE_TYPE> tuple;
         tuple.row = edge.src_index;
         tuple.col = edge.dst_index;
-        if (tuple.row<0 or tuple.col){
+        if (tuple.row<0 or tuple.col<0){
           cout<<" woring index found "<<tuple.row<<"col "<<tuple.col<<endl;
         }
         tuple.value = edge.distance;
