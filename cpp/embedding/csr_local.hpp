da69cf9d527a056c7d91e45fc043cd784f6b2e67
Isuru Ranawaka <irjanith@gmail.com>
2023-12-08 21:36:58 -0500

    add fbin read format

--- cpp/embedding/csr_local.hpp
+++ cpp/embedding/csr_local.hpp
@@ -1,5 +1,6 @@
 /**
- * This implements the CSR data structure. We use MKL routines to create CSR from COO.
+ * This implements the CSR data structure. We use MKL routines to create CSR
+ * from COO.
  */
 #pragma once
 #include "../common/common.h"
@@ -26,7 +27,7 @@ template <typename T> class CSRLocal {
 public:
   MKL_INT rows, cols;
 
-  int max_nnz, num_coords=0;
+  int max_nnz, num_coords = 0;
 
   bool transpose;
 
@@ -34,8 +35,9 @@ public:
 
   CSRLocal() {}
 
-  CSRLocal(MKL_INT rows, MKL_INT cols, MKL_INT max_nnz, Tuple<T> *coords, int num_coords, bool transpose) {
-    if (num_coords>0) {
+  CSRLocal(MKL_INT rows, MKL_INT cols, MKL_INT max_nnz, Tuple<T> *coords,
+           int num_coords, bool transpose) {
+    if (num_coords > 0) {
 
       this->transpose = transpose;
       this->num_coords = num_coords;
@@ -84,100 +86,83 @@ public:
         // Additional error-handling code here
       }
 
-      MKL_INT rows_coo, cols_coo, nnz_coo, *rows_start_coo, *rows_end_coo, *col_indices_coo;
-      double *values_coo;
+      cout << " processing transpose before conversion " << transpose << endl;
+      sparse_status_t status_csr =
+          mkl_sparse_convert_csr(tempCOO, op, &tempCSR);
 
-      mkl_sparse_d_get_info(tempCOO, &rows_coo, &cols_coo, nullptr, nullptr, nullptr);
-
-      mkl_sparse_d_get_values(tempCOO, SPARSE_GET_NONZERO_VALUES, &values_coo);
-      mkl_sparse_d_get_rows(tempCOO, SPARSE_GET_NONZERO_STRUCT, &rows_start_coo);
-      mkl_sparse_d_get_rows(tempCOO, SPARSE_GET_NONZERO_STRUCT + 1, &rows_end_coo);
-      mkl_sparse_d_get_cols(tempCOO, SPARSE_GET_NONZERO_INDICES, &col_indices_coo);
-
-
-      // Print the content of the COO matrix
-      std::cout << "COO Matrix Content:" << std::endl;
-      if (rank == 0 and transpose) {
-        for (MKL_INT i = 0; i < rows_coo; ++i) {
-          for (MKL_INT j = rows_start_coo[i]; j < rows_end_coo[i]; ++j) {
-            std::cout << "Row: " << i << ", Column: " << col_indices_coo[j]
-                      << ", Value: " << values_coo[j] << std::endl;
-          }
-        }
+      if (status_csr != SPARSE_STATUS_SUCCESS) {
+        // Handle the error
+        cout << "Error in matrix conversion:" << endl;
+        // Additional error-handling code here
       }
-      //      cout<<" processing transpose before conversion "<<transpose<<endl;
-      //      sparse_status_t status_csr =
-      //          mkl_sparse_convert_csr(tempCOO, op, &tempCSR);
-      //
-      //      if (status_csr != SPARSE_STATUS_SUCCESS) {
-      //        // Handle the error
-      //        cout<<"Error in matrix conversion:"<<endl;
-      //        // Additional error-handling code here
-      //      }
-      //
-      //      cout<<" processing transpose after conversion "<<transpose<<endl;
-      //      mkl_sparse_destroy(tempCOO);
-      //
-      //      vector<MKL_INT>().swap(rArray);
-      //      vector<MKL_INT>().swap(cArray);
-      //      vector<double>().swap(vArray);
-      //
-      //      sparse_index_base_t indexing;
-      //      MKL_INT *rows_start, *rows_end, *col_idx;
-      //      double *values;
-      //      cout<<" processing transpose before export "<<transpose<<endl;
-      //      mkl_sparse_d_export_csr(tempCSR, &indexing, &(this->rows), &(this->cols),
-      //                              &rows_start, &rows_end, &col_idx, &values);
-      //
-      //      cout<<" processing transpose after export "<<transpose<<endl;
-      //
-      //      int rv = 0;
-      //      for (int i = 0; i < num_coords; i++) {
-      //
-      //        while (rv < this->rows and i >= rows_start[rv + 1]) {
-      //          rv++;
-      //        }
-      //        coords[i].row = rv;
-      //        coords[i].col = col_idx[i];
-      //        coords[i].value = static_cast<T>(values[i]);
-      //
-      //      }
-      //
-      //      cout<<" processing coords "<<transpose<<" max nns"<<max_nnz<<endl;
-      ////      assert(num_coords <= max_nnz);
-      //
-      //      (handler.get())->values.resize(max_nnz == 0 ? 1 : max_nnz);
-      //      (handler.get())->col_idx.resize(max_nnz == 0 ? 1 : max_nnz);
-      //      (handler.get())->row_idx.resize(max_nnz == 0 ? 1 : max_nnz);
-      //      (handler.get())->rowStart.resize(this->rows + 1);
-      //
-      //// Copy over row indices
-      //      #pragma omp parallel for schedule (static)
-      //      for (int i = 0; i < num_coords; i++) {
-      //        (handler.get())->row_idx[i] = coords[i].row;
-      //      }
-      //      cout<<" processing transpose before memcopy export "<<transpose<<endl; memcpy((handler.get())->values.data(), values,
-      //             sizeof(double) * max(num_coords, 1));
-      //      memcpy((handler.get())->col_idx.data(), col_idx,
-      //             sizeof(MKL_INT) * max(num_coords, 1));
-      //      memcpy((handler.get())->rowStart.data(), rows_start,
-      //             sizeof(MKL_INT) * this->rows);
-      //      cout<<" processing transpose after memcopy export "<<transpose<<endl; (handler.get())->rowStart[this->rows] = max(num_coords, 1);
-      //
-      //      cout<<" processing transpose csr  create "<<transpose<<endl;
-      //      mkl_sparse_d_create_csr(
-      //          &((handler.get())->mkl_handle), SPARSE_INDEX_BASE_ZERO, this->rows, this->cols, (handler.get())->rowStart.data(),
-      //          (handler.get())->rowStart.data() + 1, (handler.get())->col_idx.data(),
-      //          (handler.get())->values.data());
-      //      cout<<" processing transpose after csr  create "<<transpose<<endl;
-      //      mkl_sparse_destroy(tempCSR);
-      //    }
+
+      cout << " processing transpose after conversion " << transpose << endl;
+      mkl_sparse_destroy(tempCOO);
+
+      vector<MKL_INT>().swap(rArray);
+      vector<MKL_INT>().swap(cArray);
+      vector<double>().swap(vArray);
+
+      sparse_index_base_t indexing;
+      MKL_INT *rows_start, *rows_end, *col_idx;
+      double *values;
+      cout << " processing transpose before export " << transpose << endl;
+      mkl_sparse_d_export_csr(tempCSR, &indexing, &(this->rows), &(this->cols),
+                              &rows_start, &rows_end, &col_idx, &values);
+
+      cout << " processing transpose after export " << transpose <<" size of rows_start"<<(*rows_start).size() endl;
+
+      int rv = 0;
+      for (int i = 0; i < num_coords; i++) {
+
+        while (rv < this->rows and i >= rows_start[rv + 1]) {
+          rv++;
+        }
+        coords[i].row = rv;
+        coords[i].col = col_idx[i];
+        coords[i].value = static_cast<T>(values[i]);
+      }
+
+      cout << " processing coords " << transpose << " max nns" << max_nnz
+           << endl;
+      //      assert(num_coords <= max_nnz);
+
+      (handler.get())->values.resize(max_nnz == 0 ? 1 : max_nnz);
+      (handler.get())->col_idx.resize(max_nnz == 0 ? 1 : max_nnz);
+      (handler.get())->row_idx.resize(max_nnz == 0 ? 1 : max_nnz);
+      (handler.get())->rowStart.resize(this->rows + 1);
+
+      // Copy over row indices
+#pragma omp parallel for schedule(static)
+      for (int i = 0; i < num_coords; i++) {
+        (handler.get())->row_idx[i] = coords[i].row;
+      }
+      cout << " processing transpose before memcopy export " << transpose
+           << endl;
+      memcpy((handler.get())->values.data(), values,
+             sizeof(double) * max(num_coords, 1));
+      memcpy((handler.get())->col_idx.data(), col_idx,
+             sizeof(MKL_INT) * max(num_coords, 1));
+      memcpy((handler.get())->rowStart.data(), rows_start,
+             sizeof(MKL_INT) * this->rows);
+      cout << " processing transpose after memcopy export " << transpose
+           << endl;
+      (handler.get())->rowStart[this->rows] = max(num_coords, 1);
+
+      cout << " processing transpose csr  create " << transpose << endl;
+      mkl_sparse_d_create_csr(
+          &((handler.get())->mkl_handle), SPARSE_INDEX_BASE_ZERO, this->rows,
+          this->cols, (handler.get())->rowStart.data(),
+          (handler.get())->rowStart.data() + 1, (handler.get())->col_idx.data(),
+          (handler.get())->values.data());
+      cout << " processing transpose after csr  create " << transpose << endl;
+      mkl_sparse_destroy(tempCSR);
     }
   }
 
   ~CSRLocal() {
-//    mkl_sparse_destroy((handler.get())->mkl_handle);
+    //    mkl_sparse_destroy((handler.get())->mkl_handle);
   }
 };
 
-} // namespace distblas::core
+} // namespace hipgraph::distviz::embedding
