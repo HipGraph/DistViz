6414b3888d385ddfca52cec0655400d75ac1410e
Isuru Ranawaka <irjanith@gmail.com>
2023-12-10 16:01:59 -0500

    remove mkl baxed CSR creation

--- cpp/embedding/algo.hpp
+++ cpp/embedding/algo.hpp
@@ -286,7 +286,7 @@ public:
 
     int proc_length = get_proc_length(beta, grid->col_world_size);
     int prev_start = comm_initial_start;
-    cout << " rank " << grid->rank_in_col << " run pull model algorithm "  << endl;
+//    cout << " rank " << grid->rank_in_col << " run pull model algorithm "  << endl;
     for (int k = prev_start; k < grid->col_world_size; k += proc_length) {
       int end_process = get_end_proc(k, beta, grid->col_world_size);
 
