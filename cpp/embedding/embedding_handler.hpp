9e5b3988e6f395d3ae9e67312ee6e63fc0edce52
Isuru Ranawaka <irjanith@gmail.com>
2023-12-09 21:40:54 -0500

    remove mkl baxed CSR creation

--- cpp/embedding/embedding_handler.hpp
+++ cpp/embedding/embedding_handler.hpp
@@ -67,18 +67,18 @@ public:
 
     cout<<" rank "<<grid->rank_in_col<<" CSR shared_sparseMat_receiver initialization completed "<<shared_sparseMat.get()->coords->size()<<endl;
 
-    shared_sparseMat_receiver.get()->print_csr();
+//    shared_sparseMat_receiver.get()->print_csr();
 
 
-//    unique_ptr<EmbeddingAlgo<INDEX_TYPE, VALUE_TYPE, dimension>>
-//
-//        embedding_algo = unique_ptr<EmbeddingAlgo<INDEX_TYPE, VALUE_TYPE, dimension>>(
-//                new EmbeddingAlgo<INDEX_TYPE, VALUE_TYPE, dimension>(
-//                    shared_sparseMat.get(), shared_sparseMat_receiver.get(),
-//                    shared_sparseMat_sender.get(), dense_output, grid,
-//                    alpha, beta, 5, -5,col_major,sync_comm));
-//
-//   vector<VALUE_TYPE> error_convergence = embedding_algo.get()->algo_force2_vec_ns(iterations, batch_size, nsamples, lr,drop_out_error_threshold);
+    unique_ptr<EmbeddingAlgo<INDEX_TYPE, VALUE_TYPE, dimension>>
+
+        embedding_algo = unique_ptr<EmbeddingAlgo<INDEX_TYPE, VALUE_TYPE, dimension>>(
+                new EmbeddingAlgo<INDEX_TYPE, VALUE_TYPE, dimension>(
+                    shared_sparseMat.get(), shared_sparseMat_receiver.get(),
+                    shared_sparseMat_sender.get(), dense_output, grid,
+                    alpha, beta, 5, -5,col_major,sync_comm));
+
+   vector<VALUE_TYPE> error_convergence = embedding_algo.get()->algo_force2_vec_ns(iterations, batch_size, nsamples, lr,drop_out_error_threshold);
 
 //   cout<<" rank  "<<grid->rank_in_col<<"erros #####"<<endl;
 //   if (grid->rank_in_col==0) {
