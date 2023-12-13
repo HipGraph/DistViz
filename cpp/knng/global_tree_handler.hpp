f8b684b74d3e976e4ba2e0c1c38697e36aa4b031
Isuru Ranawaka <irjanith@gmail.com>
2023-12-12 21:12:53 -0500

    remove mkl baxed CSR creation

--- cpp/knng/global_tree_handler.hpp
+++ cpp/knng/global_tree_handler.hpp
@@ -231,23 +231,23 @@ public:
 
     // calculation of bins
 //        int no_of_bins = 1 + (3.322 * log2(minimum_vector_size));
-    int no_of_bins = 7;
-
-    //calculation of distributed median
-    VALUE_TYPE *result = mathOp_ptr.get()->distributed_median (data, local_data_row_count, current_nodes,
-                                                   global_data_row_count,
-                                                   no_of_bins,
-                                                   StorageFormat::RAW, grid->rank_in_col);
-
-    VALUE_TYPE median_q_select  = mathOp_ptr.get()->distributed_median_quick_select(data, local_data_row_count, current_nodes,
-                                                      global_data_row_count,
-                                                      no_of_bins,
-                                                      StorageFormat::RAW,grid);
+//    int no_of_bins = 7;
+//
+//    //calculation of distributed median
+//    VALUE_TYPE *result = mathOp_ptr.get()->distributed_median (data, local_data_row_count, current_nodes,
+//                                                   global_data_row_count,
+//                                                   no_of_bins,
+//                                                   StorageFormat::RAW, grid->rank_in_col);
+//
+//    VALUE_TYPE median_q_select  = mathOp_ptr.get()->distributed_median_quick_select(data, local_data_row_count, current_nodes,
+//                                                      global_data_row_count,
+//                                                      no_of_bins,
+//                                                      StorageFormat::RAW,grid);
 
 //   if (grid->rank_in_col==0){
-     cout<<" rank "<<grid->rank_in_col<<" median for depth "<<depth<<" median "<< (*result)<<"q_select median"<<median_q_select<<endl;
+//     cout<<" rank "<<grid->rank_in_col<<" median for depth "<<depth<<" median "<< (*result)<<"q_select median"<<median_q_select<<endl;
 //   }
-      *result = median_q_select;
+//      *result = median_q_select;
 
     for (int i = 0; i < current_nodes; i++)
     {
@@ -257,14 +257,30 @@ public:
       int selected_leaf_left = left_index - (1 << (tree_depth - 1)) + 1;
       int selected_leaf_right = selected_leaf_left + 1;
 
-      VALUE_TYPE median = result[i];
+      vector<DataNode<INDEX_TYPE,VALUE_TYPE>> data_vector = child_data_tracker[split_starting_index + i];
+      vector<VALUE_TYPE> data(data_vector.size());
+
+     #pragma omp parallel for
+      for (int j = 0; j < data_vector.size (); j++)
+      {
+        data[j] = data_vector[j].value;
+      }
+
+
+
+      VALUE_TYPE median = mathOp_ptr.get()->distributed_median_quick_select(data, local_data_row_count, current_nodes,
+                                                                            global_data_row_count,
+                                                                            no_of_bins,
+                                                                            StorageFormat::RAW,grid);
+
+      cout<<" rank "<<grid->rank_in_col<<" median for depth "<<depth<<" median "<< (*result[i])<<"q_select median"<<median<<endl;
 
       //store median in tree_splits
       (*trees_splits_ptr)[tree][split_starting_index + i] = median;
 
       vector<DataNode<INDEX_TYPE,VALUE_TYPE>> left_childs_global;
       vector<DataNode<INDEX_TYPE,VALUE_TYPE>> right_childs_global;
-      vector<DataNode<INDEX_TYPE,VALUE_TYPE>> data_vector = child_data_tracker[split_starting_index + i];
+
 #pragma omp parallel
       {
         vector <DataNode<INDEX_TYPE,VALUE_TYPE>> left_childs;
