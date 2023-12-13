14f7cd9b891cc23c19453465be9ab90d42b60447
Isuru Ranawaka <irjanith@gmail.com>
2023-12-12 21:45:15 -0500

    remove mkl baxed CSR creation

--- cpp/knng/math_operations.hpp
+++ cpp/knng/math_operations.hpp
@@ -349,6 +349,14 @@ class MathOp {
     auto k_global = (all_medians.size()%2==0)?all_medians.size()/2:all_medians.size()/2+1;
     VALUE_TYPE global = select_k(k_global,all_medians);
     cout<<" rank "<<grid->rank_in_col<< " local median "<<local_median<<" global median "<<global<<" local max "<<max_value<<" data vector size "<<data.size()<<endl;
+
+    if (local_median==0 and max_value==0){
+      for(int i=0;i<data.size();i++){
+        cout<<" i "<<data[i]<<" ";
+      }
+      cout<<endl;
+    }
+
     return global;
   }
 
