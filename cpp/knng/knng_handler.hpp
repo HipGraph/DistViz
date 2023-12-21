061f614457fccdfe70ffcfe38fee96dcecf91e26
Isuru Ranawaka <irjanith@gmail.com>
2023-12-21 15:05:21 -0500

    remove mkl baxed CSR creation

--- cpp/knng/knng_handler.hpp
+++ cpp/knng/knng_handler.hpp
@@ -139,7 +139,7 @@ public:
 //                                                                                       local_nn_map_ptr.get(),
 //                                                                                       nn);
 
-    Eigen::MatrixXf data_matrix =  drpt_global.collect_similar_data_points_of_all_trees(use_locality_optimization,
+     drpt_global.collect_similar_data_points_of_all_trees(use_locality_optimization,
                                                                                        process_to_index_set_ptr.get(),
                                                                                        datamap_ptr.get(),
                                                                                        local_nn_map_ptr.get(),
