493e664331de55ab85f1efd6d7b7f0fa833bae97
Isuru Ranawaka <irjanith@gmail.com>
2023-12-21 14:41:33 -0500

    remove mkl baxed CSR creation

--- cpp/knng/global_tree_handler.hpp
+++ cpp/knng/global_tree_handler.hpp
@@ -692,7 +692,7 @@ public:
 //////    std::shared_ptr<Eigen::MatrixXf> matrixPtr = std::make_shared<Eigen::MatrixXf>(rows, data_dimension);
 ////
 
-    Eigen::Map<Eigen::MatrixXf> data_matrix(existingData, 3, 3);
+//    Eigen::Map<Eigen::MatrixXf> data_matrix(existingData, 3, 3);
      Eigen::MatrixXf  data_matrix(data_dimension, rows);
     auto total_data_count=0;
     for (auto it = (*process_to_index_set_ptr)[grid->rank_in_col].begin();it != (*process_to_index_set_ptr)[grid->rank_in_col].end(); ++it) {
