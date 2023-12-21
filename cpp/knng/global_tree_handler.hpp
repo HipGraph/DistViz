2567dadac098273bf25a06a3b27a307d56d6646c
Isuru Ranawaka <irjanith@gmail.com>
2023-12-21 12:52:04 -0500

    remove mkl baxed CSR creation

--- cpp/knng/global_tree_handler.hpp
+++ cpp/knng/global_tree_handler.hpp
@@ -690,12 +690,12 @@ public:
     auto rows= (*process_to_index_set_ptr)[grid->rank_in_col].size()+total_receive_count;
 //////    std::shared_ptr<Eigen::MatrixXf> matrixPtr = std::make_shared<Eigen::MatrixXf>(rows, data_dimension);
 ////
-    Eigen::MatrixXf data_matrix(data_dimension, rows);
+    Eigen::MatrixXf* data_matrix_ptr = new Eigen::MatrixXf data_matrix(data_dimension, rows);
     auto total_data_count=0;
     for (auto it = (*process_to_index_set_ptr)[grid->rank_in_col].begin();it != (*process_to_index_set_ptr)[grid->rank_in_col].end(); ++it) {
       for(int j=0;j<data_dimension;j++){
         auto index_trying = (*it) - starting_data_index;
-        data_matrix(j,total_data_count)= (*data_points_ptr)[index_trying][j];
+        (*data_matrix)(j,total_data_count)= (*data_points_ptr)[index_trying][j];
       }
       (*local_to_global_map)[total_data_count]=*it;
 
@@ -712,7 +712,7 @@ public:
       (*local_nn_map)[receive_index] = vector<EdgeNode<INDEX_TYPE,VALUE_TYPE>>(nn);
       for( auto j =0;j<data_dimension;j++){
         auto access_index = i*data_dimension+j;
-        data_matrix(j,total_data_count) =  (*receive_values_ptr)[access_index];
+        (*data_matrix)(j,total_data_count) =  (*receive_values_ptr)[access_index];
       }
       total_data_count++;
     }
