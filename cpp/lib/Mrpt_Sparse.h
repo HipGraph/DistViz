92c89d88cd714cf0367d73efdefe61d83ce857f2
Isuru Ranawaka <irjanith@gmail.com>
2024-04-18 16:54:38 -0400

    MRPT sparse

--- cpp/lib/Mrpt_Sparse.h
+++ cpp/lib/Mrpt_Sparse.h
@@ -1654,7 +1654,7 @@ private:
     int depth_min = depth - recalls.size() + 1;
     std::vector<std::vector<int>> start_indices(n_trees);
 
-//#pragma omp parallel for
+#pragma omp parallel for
     for (int n_tree = 0; n_tree < n_trees; ++n_tree) {
       start_indices[n_tree] = std::vector<int>(depth - depth_min + 1);
       int idx_tree = 0;
@@ -1664,7 +1664,6 @@ private:
         const int idx_right = idx_left + 1;
         const float split_point = split_points(idx_tree, n_tree);
         if (projected_query(j) <= split_point) {
-          std::cout<<" j "<<j<<"projected_query(j) "<<projected_query(j)<<" split_point "<<split_point<<std::endl;
           idx_tree = idx_left;
         } else {
           idx_tree = idx_right;
