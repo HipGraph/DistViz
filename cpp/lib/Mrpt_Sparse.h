81b052e3d7be1e7196d1a87d6ebbc17c71d6c4a8
Isuru Ranawaka <irjanith@gmail.com>
2024-04-18 16:08:35 -0400

    MRPT sparse

--- cpp/lib/Mrpt_Sparse.h
+++ cpp/lib/Mrpt_Sparse.h
@@ -1031,8 +1031,7 @@ public:
   }
 
   void query_sparse(Eigen::SparseVector<float> &q, int *out,
-             float *out_distances = nullptr, int vote_threshold=votes,
-             int *out_n_elected = nullptr) const {
+             float *out_distances = nullptr, int *out_n_elected = nullptr) const {
     if (index_type == normal) {
       throw std::logic_error("The index is not autotuned: k and vote threshold "
                              "has to be specified.");
@@ -1045,7 +1044,7 @@ public:
     if (k <= 0 || k > n_samples) {
       throw std::out_of_range("k must belong to the set {1, ..., n}.");
     }
-
+    int vote_threshold = votes;
     if (vote_threshold <= 0 || vote_threshold > n_trees) {
       throw std::out_of_range(
           "vote_threshold must belong to the set {1, ... , n_trees}.");
