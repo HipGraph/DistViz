8031d980661c2f570e1a617130c8669a78d6aa41
Isuru Ranawaka <irjanith@gmail.com>
2024-04-23 19:53:39 -0400

    revert distance calculation

--- cpp/embedding/algo.hpp
+++ cpp/embedding/algo.hpp
@@ -596,7 +596,7 @@ public:
             DENT d1 = -2.0 / (1.0 + attrc);
 
             for (int d = 0; d < embedding_dim; d++) {
-              DENT l = scale(forceDiff[d] * d1*exp(-1*distance));
+              DENT l = scale(forceDiff[d] * d1);
 
               (*prevCoordinates)[index * embedding_dim + d] =
                   (*prevCoordinates)[index * embedding_dim + d] + (lr)*l;
