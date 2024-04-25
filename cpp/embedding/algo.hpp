5960145d77ef54cbe027e817efab5f17ac99853a
Isuru Ranawaka <irjanith@gmail.com>
2024-04-25 10:50:31 -0400

    revert distance calculation

--- cpp/embedding/algo.hpp
+++ cpp/embedding/algo.hpp
@@ -755,7 +755,7 @@ public:
       if (csr_handle != nullptr) {
         for (uint64_t j =static_cast<uint64_t>(csr_handle->rowStart[node_index]);
              j < static_cast<uint64_t>(csr_handle->rowStart[node_index + 1]);j++) {
-         auto distance = max(0, csr_handle->values[j]);
+         auto distance = max(0.0, csr_handle->values[j]);
           value += exp(-1 * distance / mid);
         }
       }else {
