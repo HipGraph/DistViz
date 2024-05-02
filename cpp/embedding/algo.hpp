b89197d74ab46d85439a830cac15edacf4079df3
Isuru Ranawaka <irjanith@gmail.com>
2024-05-02 10:53:45 -0400

    update algo

--- cpp/embedding/algo.hpp
+++ cpp/embedding/algo.hpp
@@ -642,17 +642,18 @@ public:
       samples_per_epoch_next[i].resize(nn);
       samples_per_epoch_negative[i].resize(nn);
       samples_per_epoch_negative_next[i].resize(nn);
-//      for(uint64_t j = static_cast<uint64_t>(csr_handle->rowStart[i]);
-//           j < static_cast<uint64_t>(csr_handle->rowStart[i + 1]); j++){
-//       DENT value = csr_handle->values[j];
-//       DENT n_samples = static_cast<DENT>(iterations) * (value / maxElement);
-//       if(n_samples>0){
-//         samples_per_epoch[i][j] =  static_cast<DENT>(iterations) /n_samples;
-//         samples_per_epoch_next[i][j] =  samples_per_epoch_next[i][j];
-//         samples_per_epoch_negative[i][j] =  samples_per_epoch[i][j]/ns;
-//         samples_per_epoch_negative_next[i][j] =  samples_per_epoch_negative[i][j];
-//       }
-//      }
+      for(uint64_t j = static_cast<uint64_t>(csr_handle->rowStart[i]);
+           j < static_cast<uint64_t>(csr_handle->rowStart[i + 1]); j++){
+       DENT value = csr_handle->values[j];
+       DENT n_samples = static_cast<DENT>(iterations) * (value / maxElement);
+       if(n_samples>0){
+         int index = i -static_cast<uint64_t>(csr_handle->rowStart[i]);
+         samples_per_epoch[i][index] =  static_cast<DENT>(iterations) /n_samples;
+         samples_per_epoch_next[i][index] =  samples_per_epoch_next[i][index];
+         samples_per_epoch_negative[i][index] =  samples_per_epoch[i][index]/ns;
+         samples_per_epoch_negative_next[i][index] =  samples_per_epoch_negative[i][index];
+       }
+      }
     }
   }
 
