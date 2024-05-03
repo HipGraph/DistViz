7aab665d592e6218c0dc6514e7947910a08ac4fa
Isuru Ranawaka <irjanith@gmail.com>
2024-05-03 08:18:04 -0400

    update algo

--- cpp/embedding/algo.hpp
+++ cpp/embedding/algo.hpp
@@ -728,7 +728,7 @@ public:
          int index = j -static_cast<int>(csr_handle->rowStart[i]);
          samples_per_epoch[i][index] =  static_cast<DENT>(iterations) /n_samples;
        }else {
-         samples_per_epoch[i][index] = -1;
+         samples_per_epoch[i][index] = -1.0;
        }
        samples_per_epoch_next[i][index] =  samples_per_epoch[i][index];
        samples_per_epoch_negative[i][index] =  samples_per_epoch[i][index]/ns;
