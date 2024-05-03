e13437ab458c092e3ef7e598eb781745c53d312a
Isuru Ranawaka <irjanith@gmail.com>
2024-05-03 10:26:52 -0400

    update algo

--- cpp/embedding/algo.hpp
+++ cpp/embedding/algo.hpp
@@ -757,6 +757,7 @@ public:
 
           int ns = (iteration - samples_per_epoch_negative_next[i][index]) /samples_per_epoch_negative[i][index];
           if (ns > 0) {
+            seed += i+index;
             vector<SPT> random_number_vec = generate_random_numbers<SPT>(
                 0, (this->sp_local_receiver)->gRows, seed, ns);
             (*negative_samples_ptr)[access_index][index] = random_number_vec;
