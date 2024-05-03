a95e3302af3feed9e09c0b88b13db8166095ede0
Isuru Ranawaka <irjanith@gmail.com>
2024-05-03 09:11:54 -0400

    update algo

--- cpp/embedding/algo.hpp
+++ cpp/embedding/algo.hpp
@@ -568,7 +568,7 @@ public:
 
     int row_base_index = batch_id * batch_size;
 
-//#pragma omp parallel for schedule(static)
+#pragma omp parallel for schedule(static)
     for (int i = 0; i < block_size; i++) {
       uint64_t row_id = static_cast<uint64_t>(i + row_base_index);
       DENT forceDiff[embedding_dim];
