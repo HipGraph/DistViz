e5c35090f5246588f5e2fefa1e14bddfb9268ac2
Isuru Ranawaka <irjanith@gmail.com>
2024-05-31 13:01:31 -0400

    adding PETSc

--- cpp/embedding/algo.hpp
+++ cpp/embedding/algo.hpp
@@ -349,7 +349,7 @@ public:
                                  dst_start_index, dst_end_index, csr_block,
                                  prevCoordinates, lr, batch_id, batch_size,
                                  block_size, fetch_from_temp_cache);
-      }
+
     } else {
       for (int r = start_process; r < end_process; r++) {
 
