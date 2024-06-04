f56904dd7cf3470409dec3eaffddc05780382786
Isuru Ranawaka <irjanith@gmail.com>
2024-06-04 13:02:38 -0400

    fix file reader bug

--- cpp/lib/Mrpt_Sparse.h
+++ cpp/lib/Mrpt_Sparse.h
@@ -1390,8 +1390,8 @@ private:
         int leaf_begin = leaf_first_indices[leaf_i];
         int leaf_end = leaf_first_indices[leaf_i+1];
         const std::vector<int> &indices = tree_leaves[n_tree];
-        for (int i = leaf_begin; i < leaf_end; ++i) {
-          int idx = indices[i];
+        for (int j = leaf_begin; j < leaf_end; ++j) {
+          int idx = indices[j];
           index_to_tree_leaf_match[idx][n_tree] = leaf_i;
         }
       }
