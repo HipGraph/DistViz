a09ca0938e7bd37588ceb7ba2db425fa27fe2797
Isuru Ranawaka <irjanith@gmail.com>
2024-06-06 12:42:58 -0400

    fix file reader bug

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -206,6 +206,7 @@ int main(int argc, char* argv[]) {
 
   MPI_Barrier(MPI_COMM_WORLD);
   stop_clock_and_add(t, "IO Time");
+  t = start_clock();
   std::cout << "calling data loading completed "<<rank<<" "<<std::endl;
 
 
