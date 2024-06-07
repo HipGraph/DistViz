79c0f3c9f38c730e9f2a5e976756aebd35653b70
Isuru Ranawaka <irjanith@gmail.com>
2024-06-06 22:48:02 -0400

    fix file reader bug

--- cpp/embedding/algo.hpp
+++ cpp/embedding/algo.hpp
@@ -615,16 +615,16 @@ public:
         double sigma = smooth_knn_distance(i,nn,csr_handle);
         cout<<" sigma completed for index "<<i<<" "<<sigma<<" "<<endl;
         double value=1.0;
-//        for(uint64_t j = static_cast<uint64_t>(csr_handle->rowStart[i]);
-//             j < static_cast<uint64_t>(csr_handle->rowStart[i + 1]); j++){
+        for(uint64_t j = static_cast<uint64_t>(csr_handle->rowStart[i]);
+             j < static_cast<uint64_t>(csr_handle->rowStart[i + 1]); j++){
 //          if ((csr_handle->values[j]-minimum_dis_cache[j])<=0 or sigma==0){
 //            value = 1.0;
 //          }else {
 //            value = exp(-1*(csr_handle->values[j]-minimum_dis_cache[j])/sigma);
 //
 //          }
-//          csr_handle->values[j]=value;
-//        }
+          csr_handle->values[j]=value;
+        }
     }
   }
 
