4bff72a21588e6e7b5ed665016ebcce1e784f974
Isuru Ranawaka <irjanith@gmail.com>
2024-06-10 16:09:23 -0400

    fix file reader bug

--- cpp/embedding/algo.hpp
+++ cpp/embedding/algo.hpp
@@ -220,10 +220,10 @@ public:
           generate_negative_samples(negative_samples_ptr_count.get(),csr_handle,i,j,batch_size,
                                     considering_batch_size,seed);
 
-          this->calc_t_dist_replus_rowptr(
-              prevCoordinates_ptr.get(), negative_samples_ptr_count.get(),
-              csr_handle,alpha, j, batch_size,
-              considering_batch_size);
+//          this->calc_t_dist_replus_rowptr(
+//              prevCoordinates_ptr.get(), negative_samples_ptr_count.get(),
+//              csr_handle,alpha, j, batch_size,
+//              considering_batch_size);
 
           batch_error += this->update_data_matrix_rowptr(
               prevCoordinates_ptr.get(), j, batch_size);
