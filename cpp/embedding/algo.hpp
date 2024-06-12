cbefbbced75aadcbeb7bd336b3cfcc2e2251eb5a
Isuru Ranawaka <irjanith@gmail.com>
2024-06-12 18:03:41 -0400

    fix file reader bug

--- cpp/embedding/algo.hpp
+++ cpp/embedding/algo.hpp
@@ -518,7 +518,7 @@ public:
                     DENT d1 = 2.0 / ((repuls + 0.000001) * (1.0 + repuls));
                     for (int d = 0; d < embedding_dim; d++) {
                       forceDiff[d] = scale(forceDiff[d] * d1);
-                      (*prevCoordinates)[i * embedding_dim + d] += (lr)*forceDiff[d]*5;
+                      (*prevCoordinates)[i * embedding_dim + d] += (lr)*forceDiff[d]*3;
                     }
       }
     }
