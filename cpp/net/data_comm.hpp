2efba60d10ceae279671b664b567552a88ade09d
Isuru Ranawaka <irjanith@gmail.com>
2024-06-21 16:36:43 -0400

    fix file reader bug

--- cpp/net/data_comm.hpp
+++ cpp/net/data_comm.hpp
@@ -308,7 +308,7 @@ public:
           if ((data_handler->rowStart[i + 1] - data_handler->rowStart[i]) > 0) {
             int id = (i+iteration)%this->sp_local_receiver->gRows;
             int target_proc = id/this->sp_local_receiver->proc_row_width;
-            cout<<" rank "<<grid->rank_in_col<<" i "<<i<<" id "<<id<<" target rank "<<target_proc<<endl;
+            cout<<" rank "<<grid->rank_in_col<<" i "<<i<<" id "<<id<<" target rank "<<target_proc<<"total count"<<(data_handler->rowStart[i + 1] - data_handler->rowStart[i])<<endl;
             proc_count[target_proc] += (data_handler->rowStart[i + 1] - data_handler->rowStart[i]);
             if (target_proc != grid->rank_in_col) {
               (*sendcounts)[target_proc]++;
