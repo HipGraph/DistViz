5482e95e2fe21d1bec211a1f97c35484cb3c6e4b
Isuru Ranawaka <irjanith@gmail.com>
2024-06-25 12:32:08 -0400

    cleanup

--- cpp/net/data_comm.hpp
+++ cpp/net/data_comm.hpp
@@ -429,7 +429,7 @@ public:
 
       for (auto j = base_index; j < base_index + count; j++) {
         DataTuple<DENT, embedding_dim> t = (*receivebuf)[j];
-        (this->dense_local)->insert_cache(i, t.col, batch_id, iteration, t.value, temp);
+//        (this->dense_local)->insert_cache(i, t.col, batch_id, iteration, t.value, temp);
       }
     }
     receivebuf->clear();
