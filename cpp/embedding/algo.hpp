77152f7ab101e73541b17856eab0ba28bd6f91b1
Isuru Ranawaka <irjanith@gmail.com>
2024-07-01 15:39:14 -0400

    adding DISTFNE readme

--- cpp/embedding/algo.hpp
+++ cpp/embedding/algo.hpp
@@ -795,7 +795,7 @@ public:
         std::copy(tempMatrix.innerIndexPtr(), tempMatrix.innerIndexPtr() + nnz, col_indices.begin());
         std::copy(tempMatrix.valuePtr(), tempMatrix.valuePtr() + nnz, values.begin());
 
-        FileWriter<INDEX_TYPE,VALUE_TYPE> fileWriter;
+        FileWriter<SPT,DENT> fileWriter;
         fileWriter.parallel_write_csr("/global/homes/i/isjarana/distviz_executions/perf_comparison/DistViz/MNIST/transpose.txt",row_offsets,col_indices,values,sp_local_receiver->proc_row_width);
 
 
