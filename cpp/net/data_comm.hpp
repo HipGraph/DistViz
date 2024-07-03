6ab2486d2b52dbeeebd8ecca78e31d3af5018d3b
Isuru Ranawaka <irjanith@gmail.com>
2024-07-03 12:51:04 -0400

    adding DISTFNE readme

--- cpp/net/data_comm.hpp
+++ cpp/net/data_comm.hpp
@@ -483,7 +483,7 @@ public:
 //    triplets_transpose.reserve(transpose_values.size());
     ofstream fout;
     string output_path = "/global/homes/i/isjarana/distviz_executions/perf_comparison/DistViz/MNIST/"+to_string(getpid())+".txt";
-    fout.open(, std::ios_base::app);
+    fout.open(output_path, std::ios_base::app);
     for(int i=0;i<total_receive_count;i++) {
       triplets_transpose.emplace_back(static_cast<SPT>((*receive_value_ptr)[i].row),static_cast<SPT>((*receive_value_ptr)[i].col), static_cast<DENT>((*receive_value_ptr)[i].value));
       fout<<static_cast<SPT>((*receive_value_ptr)[i].row)<<"\t"<<static_cast<SPT>((*receive_value_ptr)[i].col)<<"\t"<<static_cast<DENT>((*receive_value_ptr)[i].value)<<endl;
