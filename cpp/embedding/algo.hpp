80ac0ffa59e50e769be900a25cca47bd8786659f
Isuru Ranawaka <irjanith@gmail.com>
2024-07-03 15:49:16 -0400

    adding DISTFNE readme

--- cpp/embedding/algo.hpp
+++ cpp/embedding/algo.hpp
@@ -300,9 +300,9 @@ public:
 
             CSRLocal<SPT, DENT> *csr_block_negative = negative_csr->csr_local_data.get();
             full_comm.get()->transfer_negative_sampled_data(csr_block_negative, i, j);
-//            generate_negative_samples(negative_samples_ptr_count.get(),
-//                                      csr_handle, i, j, batch_size,
-//                                      considering_batch_size, seed, max_nnz);
+            generate_negative_samples(negative_samples_ptr_count.get(),
+                                      csr_handle, i, j, batch_size,
+                                      considering_batch_size, seed, max_nnz);
 //            this->calc_t_dist_replus_rowptr(prevCoordinates_ptr.get(),
 //                                            negative_samples_ptr_count.get(),alpha, j, batch_size,
 //                                            considering_batch_size,i,negative_samples_ids.get(),repulsive_force_scaling_factor);
