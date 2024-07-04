6645fcc2ed88d53e1cb3d655e06bcb50a285fd22
Isuru Ranawaka <irjanith@gmail.com>
2024-07-04 16:24:02 -0400

    adding DISTFNE readme

--- cpp/embedding/algo.hpp
+++ cpp/embedding/algo.hpp
@@ -546,10 +546,11 @@ public:
                       unordered_map<uint64_t , CacheEntry<DENT, embedding_dim>> &arrayMap =
                           (*this->dense_local->tempCachePtr)[owner_rank];
                       std::array<DENT, embedding_dim> &colvec = arrayMap[global_col_id].value;
-//                      for (int d = 0; d < embedding_dim; d++) {
+                      for (int d = 0; d < embedding_dim; d++) {
 //                        forceDiff[d] = (this->dense_local)->nCoordinates[row_id * embedding_dim + d] - colvec[d];
-//                        repuls += forceDiff[d] * forceDiff[d];
-//                      }
+                        forceDiff[d] = colvec[d];
+                        repuls += forceDiff[d] * forceDiff[d];
+                      }
                     } else {
                       for (int d = 0; d < embedding_dim; d++) {
                         forceDiff[d] =(this->dense_local)->nCoordinates[row_id * embedding_dim + d] -
