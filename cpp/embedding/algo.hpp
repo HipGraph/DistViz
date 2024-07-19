9fe51aa3448dce4e822ddb8301d4847833be6493
Isuru Ranawaka <irjanith@gmail.com>
2024-07-19 12:34:48 -0400

    fix segmentation fault

--- cpp/embedding/algo.hpp
+++ cpp/embedding/algo.hpp
@@ -814,7 +814,7 @@ public:
         auto partitioner = unique_ptr<GlobalAdjacency1DPartitioner>(new GlobalAdjacency1DPartitioner(grid));
         cout<<" rank  start partitioning data"<<grid->rank_in_col<<endl;
 
-        partitioner.get()->partition_data<INDEX_TYPE,VALUE_TYPE>(shared_sparseMat.get());
+        partitioner.get()->partition_data<int,float>(shared_sparseMat.get());
         shared_sparseMat.get()->initialize_CSR_blocks();
 
         CSRLocal<int, float> *csr_block = shared_sparseMat.get()->csr_local_data.get();
