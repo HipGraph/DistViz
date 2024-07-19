8cad839f6933d7fad616674b6da0c50a711f8575
Isuru Ranawaka <irjanith@gmail.com>
2024-07-19 11:32:46 -0400

    fix segmentation fault

--- cpp/embedding/algo.hpp
+++ cpp/embedding/algo.hpp
@@ -870,8 +870,8 @@ public:
                   values.begin());
       }
 
-      //                  FileWriter<SPT,DENT> fileWriter;
-      //                  fileWriter.parallel_write_csr(grid,"/global/homes/i/isjarana/distviz_executions/perf_comparison/DistViz/MNIST/transpose.txt",row_offsets,col_indices,values,sp_local_receiver->proc_row_width);
+      FileWriter<SPT,DENT> fileWriter;
+      fileWriter.parallel_write_csr(grid,"/global/homes/i/isjarana/distviz_executions/perf_comparison/DistViz/MNIST/transpose.txt",row_offsets,col_indices,values,sp_local_receiver->proc_row_width);
     }
   }
 };
