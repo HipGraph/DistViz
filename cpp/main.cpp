ea92deb77d8f4fdc2c395fa28083d98d3a5b2072
Isuru Ranawaka <irjanith@gmail.com>
2024-07-23 16:48:48 -0400

    check cache

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -312,10 +312,10 @@ int main(int argc, char* argv[]) {
   auto gNNZ = data_set_size* (nn-1);
 
   std::cout << "start generating embedding "<< rank<< " rows "<<localARows<<" gNNZ "<<gNNZ <<std::endl;
-  embedding_handler->generate_embedding(knng_graph_ptr.get(),dense_mat.get(),
-                                        data_set_size,data_set_size,gNNZ,
-                                         batch_size,iterations,lr,nsamples,alpha,beta,
-                                        col_major,sync_comm,drop_out_error_threshold,ns_generation_skip_factor,repulsive_force_scaling_factor);
+//  embedding_handler->generate_embedding(knng_graph_ptr.get(),dense_mat.get(),
+//                                        data_set_size,data_set_size,gNNZ,
+//                                         batch_size,iterations,lr,nsamples,alpha,beta,
+//                                        col_major,sync_comm,drop_out_error_threshold,ns_generation_skip_factor,repulsive_force_scaling_factor);
 
   std::cout << "stop generating embedding "<< rank<< " "<<std::endl;
   stop_clock_and_add(t, "Embedding Total Time");
