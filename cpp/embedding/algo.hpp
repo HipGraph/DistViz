fc06180f20a605caa5d21087fe400812c4faa594
Isuru Ranawaka <irjanith@gmail.com>
2024-07-24 16:33:40 -0400

    check cache

--- cpp/embedding/algo.hpp
+++ cpp/embedding/algo.hpp
@@ -289,39 +289,39 @@ public:
         if (grid->col_world_size == 1) {
 
           // local computations for 1 process
-//          this->calc_t_dist_grad_rowptr(
-//              csr_block, prevCoordinates_ptr.get(), alpha, i, j, batch_size,
-//              considering_batch_size, true, false, 0, 0, false);
+          this->calc_t_dist_grad_rowptr(
+              csr_block, prevCoordinates_ptr.get(), alpha, i, j, batch_size,
+              considering_batch_size, true, false, 0, 0, false);
 
           // One process computations without MPI operations
-          generate_negative_samples(negative_samples_ptr_count.get(),
-                                    csr_handle, i, j, batch_size,
-                                    considering_batch_size, seed, max_nnz);
-
-          this->calc_t_dist_replus_rowptr(
-              prevCoordinates_ptr.get(), negative_samples_ptr_count.get(),
-              alpha, j, batch_size, considering_batch_size, i,
-              negative_samples_ids.get(), repulsive_force_scaling_factor);
+//          generate_negative_samples(negative_samples_ptr_count.get(),
+//                                    csr_handle, i, j, batch_size,
+//                                    considering_batch_size, seed, max_nnz);
+//
+//          this->calc_t_dist_replus_rowptr(
+//              prevCoordinates_ptr.get(), negative_samples_ptr_count.get(),
+//              alpha, j, batch_size, considering_batch_size, i,
+//              negative_samples_ids.get(), repulsive_force_scaling_factor);
         } else {
           CSRLocal<SPT, DENT> *csr_block_negative = negative_csr->csr_local_data.get();
           full_comm.get()->transfer_negative_sampled_data(csr_block_negative, i,j);
 //          cout<<" rank "<<grid->rank_in_col<<" transfer_negative_sampled_data completed "<<endl;
           // These operations are for more than one processes.
-//          this->execute_pull_model_computations(
-//              sendbuf_ptr.get(), update_ptr.get(), i, j,
-//              this->data_comm_cache[j].get(), csr_block, batch_size,
-//              considering_batch_size, alpha, prevCoordinates_ptr.get(), 1, true,
-//              0, true);
+          this->execute_pull_model_computations(
+              sendbuf_ptr.get(), update_ptr.get(), i, j,
+              this->data_comm_cache[j].get(), csr_block, batch_size,
+              considering_batch_size, alpha, prevCoordinates_ptr.get(), 1, true,
+              0, true);
           //            (this->dense_local)->print_cache(i);
 //          cout<<" rank "<<grid->rank_in_col<<"  attractive completed "<<endl;
-          generate_negative_samples(negative_samples_ptr_count.get(),
-                                    csr_handle, i, j, batch_size,
-                                    considering_batch_size, seed, max_nnz);
+//          generate_negative_samples(negative_samples_ptr_count.get(),
+//                                    csr_handle, i, j, batch_size,
+//                                    considering_batch_size, seed, max_nnz);
 //          cout<<" rank "<<grid->rank_in_col<<"  generate_negative_samples completed "<<endl;
-          this->calc_t_dist_replus_rowptr(
-              prevCoordinates_ptr.get(), negative_samples_ptr_count.get(),
-              alpha, j, batch_size, considering_batch_size, i,
-              negative_samples_ids.get(), repulsive_force_scaling_factor);
+//          this->calc_t_dist_replus_rowptr(
+//              prevCoordinates_ptr.get(), negative_samples_ptr_count.get(),
+//              alpha, j, batch_size, considering_batch_size, i,
+//              negative_samples_ids.get(), repulsive_force_scaling_factor);
 //          cout<<" rank "<<grid->rank_in_col<<"  calc_t_dist_replus_rowptr completed "<<endl;
 
 //          if (i==1198) {
