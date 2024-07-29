23f2257599fd0d50b89ba032f0f44c04795fc1d4
Isuru Ranawaka <irjanith@gmail.com>
2024-07-29 11:39:53 -0400

    check cache

--- cpp/embedding/algo.hpp
+++ cpp/embedding/algo.hpp
@@ -251,8 +251,8 @@ public:
 //    cout<<" rank "<<grid->rank_in_col<<" negative initialization completed "<<endl;
     stop_clock_and_add(t, "Iteration Total Time");
 
-    FileWriter<SPT,DENT,2> fileWriter;
-    fileWriter.parallel_write_csr(grid,"/global/homes/i/isjarana/distviz_executions/perf_comparison/DistViz/MNIST/csr.txt",csr_handle->rowStart,csr_handle->col_idx,csr_handle->values,sp_local_receiver->proc_row_width);
+//    FileWriter<SPT,DENT,2> fileWriter;
+//    fileWriter.parallel_write_csr(grid,"/global/homes/i/isjarana/distviz_executions/perf_comparison/DistViz/MNIST/csr.txt",csr_handle->rowStart,csr_handle->col_idx,csr_handle->values,sp_local_receiver->proc_row_width);
 
     for (int i = 0; i < iterations; i++) {
       DENT batch_error = 0;
