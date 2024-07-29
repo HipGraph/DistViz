d0ac574d9f8707fe34cf26dc1b3d32c3c120a4b6
Isuru Ranawaka <irjanith@gmail.com>
2024-07-29 14:54:12 -0400

    check cache

--- cpp/embedding/algo.hpp
+++ cpp/embedding/algo.hpp
@@ -488,7 +488,7 @@ public:
         for (uint64_t j = static_cast<uint64_t>(csr_handle->rowStart[i]);
              j < static_cast<uint64_t>(csr_handle->rowStart[i + 1]); j++) {
           int dst_index = j - static_cast<uint64_t>(csr_handle->rowStart[i]);
-          if (samples_per_epoch_next[i][dst_index] <= iteration + 1) {
+//          if (samples_per_epoch_next[i][dst_index] <= iteration + 1) {
             auto dst_id = csr_handle->col_idx[j];
             auto distance = csr_handle->values[j];
 //            if (dst_id >= dst_start_index && dst_id <= dst_end_index &&
@@ -537,7 +537,7 @@ public:
                     (*prevCoordinates)[index * embedding_dim + d] + (lr)*l;
               }
             }
-          }
+//          }
         }
       }
     }
