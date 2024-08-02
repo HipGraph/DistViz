1247cdff8e97e1700ffa7aabbd245568101d7bb1
isururanawaka <irjanith@gmail.com>
2024-08-02 12:32:00 -0400

    zero embedding checking

--- cpp/net/data_comm.hpp
+++ cpp/net/data_comm.hpp
@@ -317,7 +317,7 @@ void transfer_data(CSRLocal<SPT, DENT> *csr_block_repulsive, CSRLocal<SPT, DENT>
     int start_index = 0;
     int end_index = this->sp_local_receiver->proc_row_width;
     if (grid->rank_in_col == grid->col_world_size - 1) {
-       end_index = min(end_index, gRows-(grid->rank_in_col*this->sp_local_receiver->proc_row_width));
+       end_index = min(end_index, this->sp_local_receiver->gRows-(grid->rank_in_col*this->sp_local_receiver->proc_row_width));
     }
 
     #pragma omp parallel for
