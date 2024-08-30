2fd97c45db47e0dc616665fa7c7c1b7791f7a15c
isururanawaka <irjanith@gmail.com>
2024-08-30 17:04:57 -0400

    debugging

--- cpp/lib/Mrpt.h
+++ cpp/lib/Mrpt.h
@@ -1294,8 +1294,8 @@ class Mrpt {
       #pragma omp parallel for
       for(int i=0;i<index_to_tree_leaf_match.size();++i){
         int n_elected = 0;
-//        Eigen::VectorXi elected(elected_size);
-//        Eigen::VectorXi votes_vec = Eigen::VectorXi::Zero(n_samples);
+        Eigen::VectorXi elected(elected_size);
+        Eigen::VectorXi votes_vec = Eigen::VectorXi::Zero(n_samples);
 //        Eigen::VectorXi neighbour(k);
 //        Eigen::VectorXf distance(k);
 //        if (vote_threshold <= 0 || vote_threshold > n_trees) {
