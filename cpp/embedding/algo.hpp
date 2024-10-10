af8f3f3a6472effcfe5d1136e39af2bcb85fbc35
isururanawaka <irjanith@gmail.com>
2024-10-10 15:08:53 -0400

    fix density parameter configuration

--- cpp/embedding/algo.hpp
+++ cpp/embedding/algo.hpp
@@ -295,7 +295,7 @@ namespace hipgraph::distviz::embedding {
                         CSRLocal<SPT, DENT> *comm_block_csr = communication_csr->csr_local_data.get();
                         auto t = start_clock();
                         atttractive_comm.get()->transfer_data(comm_block_csr, i, j, false);
-                        repulsive_comm.get()->transfer_data(csr_block_negative, i, j, true);
+//                        repulsive_comm.get()->transfer_data(csr_block_negative, i, j, true);
 //                        stop_clock_and_add(t, "Embedding Communication Time");
                         this->execute_pull_model_computations(
                                 sendbuf_ptr.get(), update_ptr.get(), i, j,
