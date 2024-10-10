96c06e3bdd39e6e11e05b81bbae4ba30b362ae8d
isururanawaka <irjanith@gmail.com>
2024-10-10 11:52:40 -0400

    fix density parameter configuration

--- cpp/embedding/algo.hpp
+++ cpp/embedding/algo.hpp
@@ -199,6 +199,7 @@ namespace hipgraph::distviz::embedding {
             int subset_start = sp_local_receiver->proc_row_width * grid->rank_in_col * max_nnz;
             gen.discard(subset_start);
             cout<<" rank "<<grid->rank_in_col<<" gen discarded "<<endl;
+            MPI_Barrier(grid->col_world);
             unique_ptr < vector < vector < SPT>>> negative_samples_ids =
                                                           make_unique < vector < vector < SPT>>>(last_batch_size);
             cout<<" rank "<<grid->rank_in_col<<"  negative_samples_ids "<<endl;
