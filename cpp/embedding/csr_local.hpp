0976b1154333577681acead04b1329106e8e6d3f
isururanawaka <irjanith@gmail.com>
2025-03-17 18:42:45 -0400

    debugging csr issue

--- cpp/embedding/csr_local.hpp
+++ cpp/embedding/csr_local.hpp
@@ -94,12 +94,18 @@ namespace hipgraph::distviz::embedding {
                     for (INDEX_TYPE i = 0; i < num_coords; i++) {
                         if (dups.insert(coords[i].col).second) {
                             if (expected_col == coords[i].col) {
-//                                (handler.get())->rowStart[index] = current_row_value;
+                                if (index >= handler.get()->rowStart.size()){
+                                    cout<<" index and rowStart mismatch "<< index<<" "<<handler.get()->rowStart.size() <<endl;
+                                }
+                                (handler.get())->rowStart[index] = current_row_value;
                                 index++;
                                 expected_col++;
                             } else if (expected_col < coords[i].col) {
                                 while (expected_col <= coords[i].col) {
-//                                    (handler.get())->rowStart[index] = current_row_value;
+                                    if (index >= handler.get()->rowStart.size()){
+                                        cout<<" index and rowStart mismatch "<< index<<" "<<handler.get()->rowStart.size() <<endl;
+                                    }
+                                    (handler.get())->rowStart[index] = current_row_value;
                                     index++;
                                     expected_col++;
                                 }
@@ -111,7 +117,10 @@ namespace hipgraph::distviz::embedding {
                     }
                     if (expected_col <= this->cols) {
                         while (expected_col <= this->cols) {
-//                            (handler.get())->rowStart[index] = current_row_value;
+                            if (index >= handler.get()->rowStart.size()){
+                                cout<<" index and rowStart mismatch "<< index<<" "<<handler.get()->rowStart.size() <<endl;
+                            }
+                            (handler.get())->rowStart[index] = current_row_value;
                             index++;
                             expected_col++;
                         }
